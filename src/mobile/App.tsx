02834949176de8c3a67cd4bb63f897a25c8cd0c4
Noah Milberger <n.milberger@gmail.com>
2024-01-14 00:31:56 -0500

    Updated App.tsx to display Home component.

--- src/mobile/App.tsx
+++ src/mobile/App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { BleManager } from "react-native-ble-plx";
+import Home from "./components/Home";
 
 export default function App() {
+  const bleManager = new BleManager();
+
   return (
     <View style={styles.container}>
-      <Text>NexusVision</Text>
-      <StatusBar style="auto" />
+      <Home bleManager={bleManager} />
     </View>
   );
 }
