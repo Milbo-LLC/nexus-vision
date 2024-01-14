4bf44da00b8d58f67a9d9416b03f120833d72cd0
Noah Milberger <n.milberger@gmail.com>
2024-01-13 23:36:14 -0500

    Updated firmware to initalize AWS IoT connection and send test data to AWS.

--- src/firmware/src/main.cpp
+++ src/firmware/src/main.cpp
@@ -1,7 +1,20 @@
 #include <Arduino.h>
+#include <WiFi.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <WiFiClientSecure.h>
+#include <MQTTClient.h>
+#include <ArduinoJson.h>
+
+#include "secrets.h"
+
+// The MQTT topics that this device should publish/subscribe
+#define AWS_IOT_PUBLISH_TOPIC "esp32/pub"
+#define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
+
+WiFiClientSecure net = WiFiClientSecure();
+MQTTClient client = MQTTClient(256);
 
 // See the following function prototypes
 void setupBLE();
@@ -19,23 +32,13 @@ BLECharacteristic *pCharacteristic = nullptr;
 #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
-void setup()
+void messageHandler(String &topic, String &payload)
 {
-  Serial.begin(115200);
-  while (!Serial)
-  {
-    delay(100);
-  }
+  Serial.println("incoming: " + topic + " - " + payload);
 
-  Serial.println("Starting BLE work!");
-  setupBLE();
-}
-
-void loop()
-{
-  // Your existing code
-  Serial.println("Hello world!");
-  delay(1000);
+  //  StaticJsonDocument<200> doc;
+  //  deserializeJson(doc, payload);
+  //  const char* message = doc["message"];
 }
 
 void setupBLE()
@@ -60,4 +63,80 @@ void setupBLE()
   // Start advertising
   pServer->getAdvertising()->start();
   Serial.println("Waiting a client connection to notify...");
+}
+
+void connectAWS()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  Serial.println("Connecting to Wi-Fi");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  // Configure WiFiClientSecure to use the AWS IoT device credentials
+  net.setCACert(AWS_CERT_CA);
+  net.setCertificate(AWS_CERT_CRT);
+  net.setPrivateKey(AWS_CERT_PRIVATE);
+
+  // Connect to the MQTT broker on the AWS endpoint we defined earlier
+  client.begin(AWS_IOT_ENDPOINT, 8883, net);
+
+  // Create a message handler
+  client.onMessage(messageHandler);
+
+  Serial.print("Connecting to AWS IOT");
+
+  while (!client.connect(THINGNAME))
+  {
+    Serial.print(".");
+    delay(100);
+  }
+
+  if (!client.connected())
+  {
+    Serial.println("AWS IoT Timeout!");
+    return;
+  }
+
+  // Subscribe to a topic
+  client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
+
+  Serial.println("AWS IoT Connected!");
+}
+
+void publishMessage()
+{
+  StaticJsonDocument<200> doc;
+  doc["time"] = millis();
+  doc["test"] = "This is a test message.";
+  char jsonBuffer[512];
+  serializeJson(doc, jsonBuffer); // print to client
+
+  client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial)
+  {
+    delay(100);
+  }
+
+  Serial.println("Starting BLE work!");
+  setupBLE();
+  connectAWS();
+}
+
+void loop()
+{
+  Serial.println("Hello world!");
+  publishMessage();
+  client.loop();
+  delay(1000);
 }
