f0d4d4032e7eaae4e20dec058149cc8ad4bd99fd
Noah Milberger <n.milberger@gmail.com>
2024-01-14 22:04:36 -0500

    Started adding auth form to mobile app

--- src/mobile/App.tsx
+++ src/mobile/App.tsx
@@ -1,21 +1,32 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { BleManager } from "react-native-ble-plx";
-import Home from "./components/Home";
+// import Home from "./components/Home";
+import { NhostClient, NhostProvider, useAuth } from "@nhost/react";
+import React from "react";
+import AuthForm from "./components/auth/AuthForm";
+
+const nhost = new NhostClient({
+  subdomain: "sdonyrtjqhytgsmeuuzf",
+  region: "us-east-1",
+});
 
 export default function App() {
   const bleManager = new BleManager();
 
   return (
-    <View style={styles.container}>
-      <Home bleManager={bleManager} />
-    </View>
+    <NhostProvider nhost={nhost}>
+      <View style={styles.container}>
+        {/* <Home bleManager={bleManager} /> */}
+        <AuthForm />
+      </View>
+    </NhostProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: "100%",
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
